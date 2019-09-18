151d4f9e0cad1ae4e58c2204e2b22ad64333c6ef
VladADragos <vlad6620@yahoo.com>
2019-09-18 16:51:52 +0200

    started on part b

--- BlackJack.hs
+++ BlackJack.hs
@@ -7,20 +7,24 @@ import RunGame
 hand2 :: Hand
 hand2 = [Card Queen Spades,Card Ace Spades,Card Ace Spades,Card Ace Spades]
 
+hand3 :: Hand
+hand3 = [Card Queen Spades,Card King Spades,Card Ace Spades,Card Ace Spades]
 
 
 
--- sizeSteps :: [Int]
--- sizeSteps = [ size hand2
---              , size (Card (Numeric 2) Hearts : (Card Jack Spades : []))
---              , 1 + size (Card Jack Spades : [] )
---              , 1 + 1 + size []
---              , 1 + 1 + 0
---              , 2
---              ]
 
 
-myCard = Card (Numeric 10) Spades
+sizeSteps :: [Int]
+sizeSteps = [ size hand2
+             , size (Card (Numeric 2) Hearts : (Card Jack Spades : []))
+             , 1 + size (Card Jack Spades : [] )
+             , 1 + 1 + size []
+             , 1 + 1 + 0
+             , 2
+             ]
+
+
+myCard = Card (Numeric 9) Spades
 
 displayCard:: Card -> String 
 displayCard (Card rank suit) = show rank ++ " of " ++ show suit
@@ -30,34 +34,18 @@ display [] = []
 display  [card] = displayCard card
 display  (card:cards) = displayCard card ++ ", "++ display(cards) 
 
- 
-
-strToInt:: String -> Int
-strToInt string = read string ::Int
-
-
-getLastStr:: Rank -> String 
-getLastStr r = last(words(show(r))) 
-
-lastStrToInt r = strToInt (getLastStr r)
 
 valueRank :: Rank -> Int
-valueRank r
-    | r == Queen = 10
-    | r == Jack = 10
-    | r == King = 10
-    | r == Ace = 11
-    -- | lastStrToInt r == 0 = 10
-    | otherwise = lastStrToInt r
+valueRank (Numeric r) = r
+valueRank Ace = 11
+valueRank _ = 10
 
 myRank = rank myCard
 
 valueHand :: Hand -> Int
 valueHand [] = 0
 valueHand  [card] = valueRank (rank card)
-valueHand (card:cards) 
-    | rank card == Ace && ((numberOfAces cards) > 1 || 11 + valueHand(cards) > 21) =  1 + valueHand(cards)
-    | otherwise =  valueRank (rank card) + valueHand(cards)
+valueHand (card:cards) =  valueRank (rank card) + valueHand(cards)
 
 
 numberOfAces :: Hand -> Int
@@ -67,23 +55,39 @@ numberOfAces (card:cards)
     | otherwise = 0 + numberOfAces(cards) 
 
 
+value :: Hand -> Int
+value cards
+    | valueHand cards > 21 = valueHand cards - ((numberOfAces cards)* 10)
+    | otherwise = valueHand cards
+
+
+
 gameOver :: Hand -> Bool
-gameOver hand  = valueHand hand > 21
-
-
-
--- player1 = Guest
--- player2 = Bank
+gameOver hand  = value hand > 21
 
 
 winner :: Hand -> Hand -> Player 
 winner hand1 hand2 
-    | valueHand hand1 > valueHand hand2 && not (gameOver hand1) = Guest  
-    | valueHand hand1 < valueHand hand2 || gameOver hand1 = Bank  
-    | valueHand hand1 == valueHand hand2 = Bank  
+    | value hand1 > value hand2 && not (gameOver hand1) = Guest
+    | gameOver hand2 && not(gameOver hand1) = Guest
+    | value hand1 < value hand2 || gameOver hand1 = Bank  
+    | value hand1 == value hand2 = Bank  
 
 
 
+allPossibleRanks = [Numeric x| x<-[2..10]] ++ [Ace,Queen,Jack,King]
+allPossibleSuits = [Hearts,Spades,Diamonds,Clubs]
+
+
+fullDeck :: Deck
+fulldeck = [ Card r s | s<-allPossibleSuits,r<-allPossibleRanks]
+
+prop_size_fullDeck :: Bool
+prop_size_fullDeck = size fullDeck == 52
+
+draw :: Deck -> Hand -> (Deck, Hand)
+draw [] hand = error "draw: The deck is empty."
+draw deck hand = 
 
 
 
